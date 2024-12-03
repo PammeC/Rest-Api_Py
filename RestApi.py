51e36aa0a8a43a16463789c189e97c7ce1d516b5
PammeC <pamelajnth@gmail.com>
2024-12-03 11:27:37 -0500

    feat: :alien: Add program RestApi in python

---
+++ RestApi.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Base de datos simulada
+productos = [{"id": 1, "nombre": "Manzana", "precio": 0.5}]
+
+@app.route('/productos', methods=['GET'])
+def obtener_productos():
+    return jsonify(productos)
+
+@app.route('/productos', methods=['POST'])
+def agregar_producto():
+    nuevo_producto = request.get_json()
+    productos.append(nuevo_producto)
+    return jsonify(nuevo_producto), 201
+
+if __name__ == '__main__':
+    app.run(port=5000)
